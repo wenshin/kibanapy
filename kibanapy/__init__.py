43328b746560e9dc88b4671ea54247d1203b9dec
yanwx <yanwx@knownsec.com>
2015-06-11 17:12:19 +0800

    处理 setup.py 引用requests 的异常

--- kibanapy/__init__.py
+++ kibanapy/__init__.py
@@ -2,5 +2,10 @@ __version__ = '0.1.1'
 __release_date__ = '2015-06-09'
 
 
-from .dashboard import Dashboard
-from .visualization import Visualization
+try:
+    from .dashboard import Dashboard
+    from .visualization import Visualization
+except Exception:
+    # setup.py import module 来获取__version__ 但是如果是纯净的环境，
+    # requests包依赖是没有的，所以这里用异常包裹
+    pass
